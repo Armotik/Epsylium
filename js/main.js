ccfd2340c86000f49c8f6123fe9d5c24a1863c97
BuildTools <unconfigured@null.spigotmc.org>
2023-01-31 19:03:32 +0100

    UPDATE 31.01.2023

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ document.getElementById("scroll-icon").addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+document.getElementById("history-stickers").querySelectorAll("img").forEach(e => {
+
+  e.addEventListener("mouseover", figcaptionEvent);
+  e.addEventListener("mouseover", setMainImage);
+})
+
+function figcaptionEvent() {
+
+  document.getElementById("history-main-img-title").innerHTML = this.getAttribute("alt");
+}
+
+function setMainImage() {
+  let name = this.getAttribute("src").split("/")[2];
+  document.getElementById("history-main-img").setAttribute("src", `../img/gallery/${name}`);
+
+}
